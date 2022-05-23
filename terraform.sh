439f456bc4f59ba084b1a0da9eabce8ace125c7c
Tejeda, Engelbert <etejeda@seic.com>
2022-05-23 13:58:59 -0400

    Added diy terraform version manager

---
+++ terraform.sh
@@ -0,0 +1,77 @@
+function tvm()
+{ 
+  TERRAFORM_BIN=$(which terraform)
+  TERRAFORM_DIR=${TERRAFORM_DIR-${TERRAFORM_BIN%/*}}
+  PLATFORM=${PLATFORM-'windows_amd64'}
+    
+  if [ -z "$1" ]
+  then
+    echo -e """Current:
+    ${TERRAFORM_BIN}"""
+    return
+  fi
+  
+  if [ "$1" == "checkupdates" ]
+  then
+    curl -s https://releases.hashicorp.com/terraform/?_ga=2.242595202.1674966510.1509370400-1120994974.1509370400 | awk '/\s*<a href="\/terra/ { start=index($2,">")+1;end=index($2,"<"); tver=substr($2,start, end - start); print tver; }'
+    return
+  fi
+  
+  if [ "$1" == "install" ]
+  then
+    if [ -z "$2" ]
+    then
+      echo "You must specify a terraform version!"
+      return
+    fi
+
+    SOURCE="https://releases.hashicorp.com/terraform/$2/terraform_$2_$PLATFORM.zip"
+    TARGET="$TERRAFORM_DIR/$2"
+    CURL_OUTPUT="./terraform_$2_$PLATFORM.zip"
+
+    mkdir $TARGET
+    pushd $TARGET
+
+    echo -e " $SOURCE -> $TARGET..."
+    curl --output $CURL_OUTPUT $SOURCE
+    unzip $CURL_OUTPUT
+
+    popd
+
+    return
+  fi
+
+  if [ "$1" == "remove" ]
+  then
+    TARGET="$TERRAFORM_DIR/$2"
+
+    echo "Removing $TARGET..."
+
+    if [ -z "$2" ]
+    then
+      return
+    fi
+
+    rm -r $TARGET
+
+
+  fi
+
+  if [ ! -d "$TERRAFORM_DIR/$1" ]
+  then
+   echo -e "Terraform version $1 not found"
+   return
+  fi
+
+  echo Setting Terraform Version $1
+
+  for d in $(ls -l $TERRAFORM_DIR | awk '/^d/{print $9;}')
+  do
+    DIR="$TERRAFORM_DIR/$d"
+    ESCAPED_DIR=$(echo $DIR | sed -e 's/\//\\\//g')
+    SED_CMD="s/$ESCAPED_DIR://"
+    PATH=$(echo $PATH | sed -e "$SED_CMD")
+  done
+
+  PATH="$(echo -e "$TERRAFORM_DIR/$1/:$PATH")"  
+} 
